ae924e180c062b3c021df00438c2a1099db04293
Peter Hurford <peter@peterhurford.com>
2014-10-27 17:42:10 -0500

    only tryCatch if the user asks for it

--- R/batchman.R
+++ R/batchman.R
@@ -5,19 +5,23 @@
 #' @param ... additional arguments to pass to \code{batch_fn}.
 #' @param size numeric. The size of the packets. Default 50.
 #' @param verbose logical. Whether or not to announce progress by printing dots.
+#' @param stop logical. Whether to stop if an error is raised.
 #' @export
-batchman <- function(batch_fn, inputs, ..., size = 50, verbose = TRUE) {
+batchman <- function(batch_fn, inputs, ..., size = 50, verbose = TRUE, stop = FALSE) {
   if (length(inputs) <= size) return(batch_fn(inputs, ...))
   slices <- split(inputs, as.integer((seq_along(inputs) - 1) / size))
   batches <- lapply(seq_along(slices), function(i) {
     if (verbose) cat(".")
-    tryCatch(
-      batch_fn(slices[[i]], ...),
-      error = function(e) {
-        warning("Some of the data failed to process because: ", e$message)
-        NULL
-      }
-    )
+    if (stop) batch_fn(slices[[i]], ...)
+    else {
+      tryCatch(
+        batch_fn(slices[[i]], ...),
+        error = function(e) {
+          warning("Some of the data failed to process because: ", e$message)
+          NULL
+        }
+      )
+    }
   })
   combine_by_list(batches)
 }
