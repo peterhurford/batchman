6bbcefcab2e09f869335db95f39fd4020f4017c5
Peter Hurford <peter@peterhurford.com>
2014-10-28 19:24:29 -0500

    function returning function!

--- R/batch.R
+++ R/batch.R
@@ -1,39 +1,45 @@
 #' batch maps a function to a batched version of that function.
 #'
 #' @param batch_fn function. The method to batch over.
-#' @param inputs. The inputs to split into batches.
+#' @param splitting_strategy function. The strategy used to split up inputs.
+#' @param combination_strategy function. The strategy used to recombine batches.
 #' @param ... additional arguments to pass to \code{batch_fn}.
 #' @param size numeric. The size of the packets. Default 50.
 #' @param verbose logical. Whether or not to announce progress by printing dots.
 #' @param stop logical. Whether to stop if an error is raised.
 #' @export
-batch <- function(batch_fn, inputs, ..., splitting_strategy = NULL, combination_strategy, size = 50, verbose = TRUE, stop = TRUE) {
-  if (length(inputs) <= size) return(batch_fn(inputs, ...))
-
-  splitting_strategy <- if(is.null(splitting_strategy)) {
-    function(inputs, size) {
-      i <- 0
-      run_length <- ceiling(length(inputs) / size)
-      function() {
-        if (i >= run_length) return('batchman.is.done')
-        i <<- i+1
-        split(inputs, as.integer((seq_along(inputs) - 1) / size))[i]
+batch <- function(batch_fn, splitting_strategy = NULL, combination_strategy, size = 50, verbose = TRUE, stop = TRUE) {
+  function(...) {
+    if (length(..1) <= size) return(batch_fn(...))
+    else {
+      if(verbose) cat('More than', size, 'inputs detected.  Batching...\n')
+      splitting_strategy <- if(is.null(splitting_strategy)) {
+        function(..1, size) {
+          i <- 0
+          run_length <- ceiling(length(..1) / size)
+          function() {
+            if (i >= run_length) return('batchman.is.done')
+            i <<- i+1
+            split(..1, as.integer((seq_along(..1) - 1) / size))[i]
+          }
+        }
+      } else splitting_strategy
+      roller <- splitting_strategy(inputs, size)
+      out <- roller()
+      batches <- batch_fn(out)
+      while (out != 'batchman.is.done') {
+        if (verbose) cat('.')
+        arguments <- substitute(alist(...))
+        arguments[[2]] <- out
+        batch <- do.call(batch_fn, list(arguments))
+        tryCatch({
+          batches <<- combination_strategy(batches, batch)
+        }, error = function(e) {
+          if (stop) stop(e$message)
+          else warning('Some of the data failed to process because: ', e$message)
+        })
+        out <- roller()
       }
     }
-  } else splitting_strategy
-
-  roller <- splitting_strategy(inputs, size)
-  out <- roller()
-  batches <- batch_fn(out)
-  while (out != 'batchman.is.done') {
-    if (verbose) cat('.')
-    batch <- batch_fn(out)
-    tryCatch({
-      batches <<- combination_strategy(batches, batch)
-    }, error = function(e) {
-      if (stop) stop(e$message)
-      else warning('Some of the data failed to process because: ', e$message)
-    })
-    out <- roller()
   }
 }
