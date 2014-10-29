02bf46efb6abc18e1a5b3cf9ee8297c5e80f2beb
Peter Hurford <peter@peterhurford.com>
2014-10-29 12:27:41 -0500

    batching announcement now part of splitting strategy

--- R/batch.R
+++ R/batch.R
@@ -10,39 +10,35 @@
 #' @export
 batch <- function(batch_fn, splitting_strategy = NULL, combination_strategy, size = 50, verbose = TRUE, stop = TRUE) {
   function(...) {
-    if (length(..1) <= size) return(batch_fn(...))
-    else {
-      if(verbose) cat('More than', size, 'inputs detected.  Batching...\n')
-      splitting_strategy <- if(is.null(splitting_strategy)) batchman:::key_strategy else splitting_strategy
-      tryCatch({
-        roller <- splitting_strategy(..1, size)
-        out <- roller()
-        while (!identical(out, 'batchman.is.done')) {
-          if (verbose) cat('.')
-          arguments <- substitute(alist(...))
-          arguments[[2]] <- out
-          arguments[[1]] <- quote(batch_fn)
-          batch <- eval(arguments)
+    splitting_strategy <- if(is.null(splitting_strategy)) batchman:::key_strategy else splitting_strategy
+    tryCatch({
+      roller <- splitting_strategy(..1, size)
+      out <- roller()
+      while (!identical(out, 'batchman.is.done')) {
+        if (verbose) cat('.')
+        arguments <- substitute(alist(...))
+        arguments[[2]] <- out
+        arguments[[1]] <- quote(batch_fn)
+        batch <- eval(arguments)
 #           batch <- do.call(batch_fn, list(arguments))
-          if (exists('batches'))
-            batches <- combination_strategy(batches, batch)
-          else
-            batches <- batch
-          out <- roller()
+        if (exists('batches'))
+          batches <- combination_strategy(batches, batch)
+        else
+          batches <- batch
+        out <- roller()
+      }
+    }, error = function(e) {
+      if (stop) {
+        cat('\nERROR... HALTING.\n')
+        if(exists('batches')) {
+          batchman:::partial_progress$set(batches)
+          cat('Partial progress saved to batchman::progress()\n')
         }
-      }, error = function(e) {
-        if (stop) {
-          cat('\nERROR... HALTING.\n')
-          if(exists('batches')) {
-            batchman:::partial_progress$set(batches)
-            cat('Partial progress saved to batchman::progress()\n')
-          }
-          stop(e$message)
-        }
-        else warning('Some of the data failed to process because: ', e$message)
-      })
-    }
-    batches
+        stop(e$message)
+      }
+      else warning('Some of the data failed to process because: ', e$message)
+    })
+    if (exists('batches')) batches else out
   }
 }
 
@@ -59,9 +55,10 @@ progress <- function() batchman:::partial_progress$get()
 
 key_strategy <- function(inputs, size) {
   run_length <- length(inputs)
+  if (run_length > size) cat('More than', size, 'inputs detected.  Batching...\n')
   i <- 1
   function() {
-    if (i >= run_length) return('batchman.is.done')
+    if (i > run_length) return('batchman.is.done')
     on.exit(i <<- i + size)
     inputs[seq(i, min(i + size - 1, run_length))]
   }
