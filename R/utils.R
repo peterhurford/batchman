07caec3394df3d084ba61eb4cea0409e83e99973
Peter Hurford <peter@peterhurford.com>
2014-11-05 11:11:59 -0600

    Revert "Revert "Batchman v2""

---
+++ R/utils.R
@@ -0,0 +1,10 @@
+batches <- structure(list(), class = 'no_batches')
+done <- structure(list(), class = 'batchman.is.done')
+
+#' A helper method to determine if batches doesn't contain batches.
+#' @param batches The object to see if it contains batches.
+is.no_batches <- function(batches) is(batches, 'no_batches')
+
+#' A helper method to determine if batchman is done batching.
+#' @param call The batchman call object.
+is.done <- function(call) is(call, 'batchman.is.done')
