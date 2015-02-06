b72f990cae73586afa37b8aa2d799090905d5147
Peter Hurford <peter@peterhurford.com>
2015-02-06 11:20:19 -0600

    with robust batch don't keep going if there's nothing to do; expose retries as a var

--- R/robust-batch.R
+++ R/robust-batch.R
@@ -2,7 +2,7 @@
 #'
 #' @param batched_fn function.  The batched function to re-run more robustly.
 #' @export
-robust_batch <- function(batched_fn, ...) {
+robust_batch <- function(batched_fn, ..., batchman.retries = 3) {
   robust_batched_fn <- batchman::batch(
     batchman::get_before_fn(batched_fn),
     environment(batched_fn)$keys,
@@ -14,9 +14,10 @@ robust_batch <- function(batched_fn, ...) {
     stop = FALSE
   )
   remaining_args <- list(...)
-  for (try in seq(3)) {
+  for (try in seq(batchman.retries)) {
     run <- do.call(robust_batched_fn, remaining_args)
     remaining_args <- list(unlist(remaining_args)[is.na(run)])
+    if (length(unlist(remaning_args)) == 0) break
     if (try == 1) output <- run else output[is.na(output)] <- run
   }
   output
