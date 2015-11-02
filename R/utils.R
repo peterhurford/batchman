d75da87f32d7a33aa3ae86bb5a421920ad9f1d79
Peter Hurford <peter@peterhurford.com>
2015-11-02 16:50:41 -0600

    Move as.ordinal from batbelt into utils

--- R/utils.R
+++ R/utils.R
@@ -31,3 +31,18 @@ unbatched <- get_before_fn
 #' @param fn function. The function to print.
 #' @export
 print.batched_function <- function(fn) print(list(before_fn = get_before_fn(fn), after_fn = body(fn)))
+
+
+#' Converts a number to an ordinal (e.g., first, second, etc.)
+#' @param num numeric. The number to convert to ordinal.
+#' @export
+as.ordinal <- function(num) {
+  ordinals <- list('first', 'second', 'third', 'fourth', 'fifth',
+    'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh',
+    'twelfth', 'thirteenth', 'fourteenth', 'fifteenth',
+    'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth',
+    'twentieth')
+  ext <- c("th", "st", "nd", "rd", rep("th", 6))
+  ordinals[num][[1]] %||%
+  paste0(num, ext[[(num %% 10) + 1]])
+}
